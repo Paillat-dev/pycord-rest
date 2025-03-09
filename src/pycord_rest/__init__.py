67bfd94e56f01db4c6955a63329aa3671e50e9b7
Paillat-dev <me@paillat.dev>
2025-03-09 09:56:11 +0100

    :recycle: Remove wildcard import from discord in __init__.py

--- src/pycord_rest/__init__.py
+++ src/pycord_rest/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) Paillat-dev
 # SPDX-License-Identifier: MIT
-from discord import *  # noqa: F403, I001  # pyright: ignore [reportWildcardImportFromLibrary]
 from .app import App
 
 Bot = App
