361c28830b113fff79a55a32ae7285e6cba783f1
Paillat <me@paillat.dev>
2025-03-13 08:56:31 +0100

    :sparkles: Implement close method to properly handle resource cleanup in App class

--- src/pycord_rest/app.py
+++ src/pycord_rest/app.py
@@ -279,7 +279,10 @@ class App(discord.Bot):
 
     @override
     async def close(self) -> None:
-        pass
+        self._closed: bool = True
+
+        await self.http.close()
+        self._ready.clear()
 
     @override
     async def start(  # pyright: ignore [reportIncompatibleMethodOverride]
